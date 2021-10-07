7e1a5e0091c880db5e853e915d1bbda36bcff508
wormpilled <jtchaschowy+wormpill@gmail.com>
2021-10-07 14:49:13 -0700

    :bug:

--- src/jquery-infscroll.js
+++ src/jquery-infscroll.js
@@ -114,7 +114,7 @@
             opts.loading.selector = opts.loading.selector || opts.contentSelector;
 
             // Define loading.msg
-            opts.loading.msg = opts.loading.msg || $('<div id="infscr-loading"><img alt="xxxxoading..." src="' + opts.loading.img + '" /><div>' + opts.loading.msgText + '</div></div>');
+            opts.loading.msg = opts.loading.msg || $('');
 
             // Preload loading.img
             (new Image()).src = opts.loading.img;
