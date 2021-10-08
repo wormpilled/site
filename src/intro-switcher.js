94b18d31ad2175da7ce876eefc88efe1f6c3a821
wormpilled <jtchaschowy+wormpill@gmail.com>
2021-10-07 19:18:13 -0700

    :bug:

--- src/intro-switcher.js
+++ src/intro-switcher.js
@@ -9,11 +9,11 @@ if (systemInitiatedDark.matches) {
 
 function prefersColorTest(systemInitiatedDark) {
   if (systemInitiatedDark.matches) {
-  	document.documentElement.setAttribute('data-theme', 'dark');		
+  	document.documentElement.setAttribute('data-intro', 'dark');		
    	document.getElementById("intro-toggle").innerHTML = "Light Mode";
    	sessionStorage.setItem('introtg', '');
   } else {
-  	document.documentElement.setAttribute('data-theme', 'light');
+  	document.documentElement.setAttribute('data-intro', 'light');
     document.getElementById("intro-toggle").innerHTML = "Dark Mode";
     sessionStorage.setItem('introtg', '');
   }
@@ -24,30 +24,30 @@ systemInitiatedDark.addListener(prefersColorTest);
 function modeSwitcher() {
 	let introtg = sessionStorage.getItem('introtg');
 	if (introtg === "dark") {
-		document.documentElement.setAttribute('data-theme', 'light');
+		document.documentElement.setAttribute('data-intro', 'light');
 		sessionStorage.setItem('introtg', 'light');
 		document.getElementById("intro-toggle").innerHTML = "Dark Mode";
 	}	else if (introtg === "light") {
-		document.documentElement.setAttribute('data-theme', 'dark');
+		document.documentElement.setAttribute('data-intro', 'dark');
 		sessionStorage.setItem('introtg', 'dark');
 		document.getElementById("intro-toggle").innerHTML = "Light Mode";
 	} else if (systemInitiatedDark.matches) {	
-		document.documentElement.setAttribute('data-theme', 'light');
+		document.documentElement.setAttribute('data-intro', 'light');
 		sessionStorage.setItem('introtg', 'light');
 		document.getElementById("intro-toggle").innerHTML = "Dark Mode";
 	} else {
-		document.documentElement.setAttribute('data-theme', 'dark');
+		document.documentElement.setAttribute('data-intro', 'dark');
 		sessionStorage.setItem('introtg', 'dark');
 		document.getElementById("intro-toggle").innerHTML = "Light Mode";
 	}
 }
 
 if (introtg === "dark") {
-	document.documentElement.setAttribute('data-theme', 'dark');
+	document.documentElement.setAttribute('data-intro', 'dark');
 	sessionStorage.setItem('introtg', 'dark');
 	document.getElementById("intro-toggle").innerHTML = "Light Mode";
 } else if (introtg === "light") {
-	document.documentElement.setAttribute('data-theme', 'light');
+	document.documentElement.setAttribute('data-intro', 'light');
 	sessionStorage.setItem('introtg', 'light');
 	document.getElementById("intro-toggle").innerHTML = "Dark Mode";
 }
