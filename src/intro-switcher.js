a7f2930afe8badf9fc18cfccd51b2a86f3f1423a
wormpilled <jtchaschowy+wormpill@gmail.com>
2021-10-07 19:41:23 -0700

    :bug:

--- src/intro-switcher.js
+++ src/intro-switcher.js
@@ -2,19 +2,19 @@ let systemInitiatedHide = window.matchMedia("(prefers-color-scheme: hide)");
 let introdisplay = sessionStorage.getItem('introdisplay');
 
 if (systemInitiatedHide.matches) {
-	document.getElementById("introdisplay-toggle").innerHTML = "Show Mode";
+	document.getElementById("introdisplay-toggle").innerHTML = "Show Introduction";
 } else {
-	document.getElementById("introdisplay-toggle").innerHTML = "Hide Mode";
+	document.getElementById("introdisplay-toggle").innerHTML = "Hide Introduction";
 }
 
 function prefersColorTest(systemInitiatedHide) {
   if (systemInitiatedHide.matches) {
   	document.documentElement.setAttribute('data-intro', 'hide');		
-   	document.getElementById("introdisplay-toggle").innerHTML = "Show Mode";
+   	document.getElementById("introdisplay-toggle").innerHTML = "Show Introduction";
    	sessionStorage.setItem('introdisplay', '');
   } else {
   	document.documentElement.setAttribute('data-intro', 'show');
-    document.getElementById("introdisplay-toggle").innerHTML = "Hide Mode";
+    document.getElementById("introdisplay-toggle").innerHTML = "Hide Introduction";
     sessionStorage.setItem('introdisplay', '');
   }
 }
@@ -26,28 +26,28 @@ function introSwitcher() {
 	if (introdisplay === "hide") {
 		document.documentElement.setAttribute('data-intro', 'show');
 		sessionStorage.setItem('introdisplay', 'show');
-		document.getElementById("introdisplay-toggle").innerHTML = "Hide Mode";
+		document.getElementById("introdisplay-toggle").innerHTML = "Hide Introduction";
 	}	else if (introdisplay === "show") {
 		document.documentElement.setAttribute('data-intro', 'hide');
 		sessionStorage.setItem('introdisplay', 'hide');
-		document.getElementById("introdisplay-toggle").innerHTML = "Show Mode";
+		document.getElementById("introdisplay-toggle").innerHTML = "Show Introduction";
 	} else if (systemInitiatedHide.matches) {	
 		document.documentElement.setAttribute('data-intro', 'show');
 		sessionStorage.setItem('introdisplay', 'show');
-		document.getElementById("introdisplay-toggle").innerHTML = "Hide Mode";
+		document.getElementById("introdisplay-toggle").innerHTML = "Hide Introduction";
 	} else {
 		document.documentElement.setAttribute('data-intro', 'hide');
 		sessionStorage.setItem('introdisplay', 'hide');
-		document.getElementById("introdisplay-toggle").innerHTML = "Show Mode";
+		document.getElementById("introdisplay-toggle").innerHTML = "Show Introduction";
 	}
 }
 
 if (introdisplay === "hide") {
 	document.documentElement.setAttribute('data-intro', 'hide');
 	sessionStorage.setItem('introdisplay', 'hide');
-	document.getElementById("introdisplay-toggle").innerHTML = "Show Mode";
+	document.getElementById("introdisplay-toggle").innerHTML = "Show Introduction";
 } else if (introdisplay === "show") {
 	document.documentElement.setAttribute('data-intro', 'show');
 	sessionStorage.setItem('introdisplay', 'show');
-	document.getElementById("introdisplay-toggle").innerHTML = "Hide Mode";
+	document.getElementById("introdisplay-toggle").innerHTML = "Hide Introduction";
 }
