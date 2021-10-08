77066058c5b9d427ef1c5e99e590ef11d6cd10b1
wormpilled <jtchaschowy+wormpill@gmail.com>
2021-10-07 19:31:03 -0700

    :bug:

--- src/intro-switcher.js
+++ src/intro-switcher.js
@@ -23,11 +23,11 @@ systemInitiatedHide.addListener(prefersHideTest);
 
 function introSwitcher() {
 	let introtog = sessionStorage.getItem('intro');
-	if (introtog === "show") {
+	if (introtog === "hide") {
 		document.documentElement.setAttribute('data-intro', 'hide');
 		sessionStorage.setItem('intro', 'hide');
 		document.getElementById("intro-toggle").innerHTML = "Hide Intro";
-	}	else if (introtog === "light") {
+	}	else if (introtog === "show") {
 		document.documentElement.setAttribute('data-intro', 'show');
 		sessionStorage.setItem('intro', 'show');
 		document.getElementById("intro-toggle").innerHTML = "Show Intro";
