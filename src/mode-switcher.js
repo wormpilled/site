706b9fabda0d7e57e4e773803a2fa4ad2d2bcabf
wormpilled <jtchaschowy+wormpill@gmail.com>
2021-10-11 18:54:16 -0700

    :bug:

--- src/mode-switcher.js
+++ src/mode-switcher.js
@@ -1,19 +1,9 @@
-const toggle = document.getElementById("toggle");
-const refresh = document.getElementById("refresh");
+const toggle = document.getElementById("toggle"); 
 const theme = window.localStorage.getItem("theme");
-
-/* verifica se o tema armazenado no localStorage Ã© escuro
-se sim aplica o tema escuro ao body */
 if (theme === "dark") document.body.classList.add("dark");
-
-// event listener para quando o botÃ£o de alterar o tema for clicado
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   if (theme === "dark") {
     window.localStorage.setItem("theme", "light");
   } else window.localStorage.setItem("theme", "dark");
-});
-
-refresh.addEventListener("click", () => {
-  window.location.reload();
 });
